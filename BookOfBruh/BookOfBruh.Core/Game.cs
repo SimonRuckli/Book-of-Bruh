f04a4df9bf37753d3de259daad364a6a289bbc85
Vincent Dittli <vincent.dittli@bbv.ch>
2021-11-04 11:20:41 +0100

    added Game

---
+++ BookOfBruh/BookOfBruh.Core/Game.cs
@@ -0,0 +1,29 @@
+﻿namespace BookOfBruh.Core
+{
+    using System;
+    using BookOfBruh.Core.Code;
+
+    public class Game
+    {
+        private readonly SlotMachine slotMachine;
+        private readonly Player player;
+        private readonly CodeValidator codeValidator;
+
+        public Game(SlotMachine slotMachine, Player player, CodeValidator codeValidator)
+        {
+            this.slotMachine = slotMachine;
+            this.player = player;
+            this.codeValidator = codeValidator;
+        }
+
+        public void Spin(double stake)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddToWallet(int code)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
