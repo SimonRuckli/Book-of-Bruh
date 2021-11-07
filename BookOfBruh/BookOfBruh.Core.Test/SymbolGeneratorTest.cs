85d4f192ea1eeac41c8e20dc183e37b8f7fb0436
JoelTschopp <joel_tschopp@sluz.ch>
2021-11-07 21:17:18 +0100

    implemented SymbolGeneratorShouldReturnTenSymbolWhenZero

---
+++ BookOfBruh/BookOfBruh.Core.Test/SymbolGeneratorTest.cs
@@ -0,0 +1,24 @@
+﻿namespace BookOfBruh.Core.Test
+{
+    using Xunit;
+    using FluentAssertions;
+    using BookOfBruh.Core.Slot;
+    using BookOfBruh.Core.Symbols;
+
+    public class SymbolGeneratorTest
+    {
+        [Fact]
+        public void SymbolGeneratorShouldReturnTenSymbolWhenZero()
+        {
+            // Arrange
+            ISymbolGenerator testee = new SymbolGenerator();
+            ISymbol expected = new TenSymbol();
+
+            // Act
+            ISymbol result = testee.Generate(0);
+
+            // Assert
+            result.Should().BeEquivalentTo(expected);
+        }
+    }
+}
---
+++ BookOfBruh/BookOfBruh.Core/Slot/SymbolGenerator.cs
@@ -0,0 +1,12 @@
+﻿namespace BookOfBruh.Core.Slot
+{
+    using BookOfBruh.Core.Symbols;
+
+    public class SymbolGenerator : ISymbolGenerator
+    {
+        public ISymbol Generate(int number)
+        {
+            return new TenSymbol();
+        }
+    }
+}
