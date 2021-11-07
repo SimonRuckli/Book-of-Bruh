4019d28d5d4d99dab8bccca6ac3b66748d477d16
Vincent Dittli <vincent.dittli@bbv.ch>
2021-11-07 19:30:11 +0100

    implemented actual architecture

---
+++ BookOfBruh/BookOfBruh.Core/SpinResult.cs
@@ -0,0 +1,14 @@
+﻿namespace BookOfBruh.Core
+{
+    public class SpinResult 
+    {
+        public Slots Slots { get; }
+        public double BruhCoins { get; }
+
+        public SpinResult(Slots slots, double bruhCoins)
+        {
+            this.Slots = slots;
+            this.BruhCoins = bruhCoins;
+        }
+    }
+}
