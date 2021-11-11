20696bdc4f36ed15035b62dc544dfc10d61330ed
SimonRuckli <simon_ruckli@sluz.ch>
2021-11-11 16:25:55 +0100

    started working on slot analysation

---
+++ BookOfBruh/BookOfBruh.Core.Test/SlotAnalysation/SlotAnalysationTest.cs
@@ -0,0 +1,30 @@
+﻿namespace BookOfBruh.Core.Test.SlotAnalysation
+{
+    using Xunit;
+    using FluentAssertions;
+    using BookOfBruh.Core.SlotAnalysation;
+    using BookOfBruh.Core.GameData;
+    using BookOfBruh.Core.Symbols;
+
+    public class SlotAnalysationTest
+    {
+        [Fact]
+        public void test()
+        {
+            // Arrange
+            SlotAnalyzer testee = new SlotAnalyzer();
+            ISymbol[,] symbols = new ISymbol[5, 3]
+            {
+                {new ASymbol(), new ASymbol(), new ASymbol()},
+                {new ASymbol(), new ASymbol(), new ASymbol()},
+                {new ASymbol(), new ASymbol(), new ASymbol()},
+                {new ASymbol(), new ASymbol(), new ASymbol()},
+                {new ASymbol(), new ASymbol(), new ASymbol()},
+            };
+            Slots input = new Slots(symbols);
+            // Act
+            testee.Analyze(input);
+            // Assert
+        }
+    }
+}
