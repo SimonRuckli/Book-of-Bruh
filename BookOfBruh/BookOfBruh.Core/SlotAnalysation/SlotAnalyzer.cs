1fe5f891ee4903507ea71c7d1adc618a7087dd2f
Vincent Dittli <vincent.dittli@bbv.ch>
2021-11-15 23:59:55 +0100

    implemented ugly SlotAnalyzer logic

--- BookOfBruh/BookOfBruh.Core/SlotAnalysation/SlotAnalyzer.cs
+++ BookOfBruh/BookOfBruh.Core/SlotAnalysation/SlotAnalyzer.cs
@@ -1,4 +1,9 @@
-﻿namespace BookOfBruh.Core.SlotAnalysation
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using BookOfBruh.Core.Symbols;
+
+namespace BookOfBruh.Core.SlotAnalysation
 {
     using BookOfBruh.Core.GameData;
 
@@ -13,8 +18,57 @@
 
         public double Analyze(Slots slots)
         {
-            return 0;
+            double multiplier = 0;
+
+            for (int y = 0; y < slots.Rows; y++)
+            {
+                const int first = 0;
+                ISymbol firstSymbol = slots.Symbols[first, y];
+                List<Point> points = GetSameSymbolPoints(firstSymbol, slots);
+                points.Add(new Point(first,y));
+
+                List<Pattern> patterns = this.patternMatcher.FindMatches(points);
+
+                List<int> patternCounts = patterns.Select(pattern => pattern.Value.Count).ToList();
+
+                foreach (int patternCount in patternCounts)
+                {
+                    if (patternCount == 5)
+                    {
+                        multiplier += 24 * firstSymbol.Rarity;
+                    }
+                    if (patternCount == 4)
+                    {
+                        multiplier += 6 * firstSymbol.Rarity;
+                    }
+                    if (patternCount == 3)
+                    {
+                        multiplier += 3 * firstSymbol.Rarity;
+                    }
+                }
+            }
+
+            return multiplier;
         }
 
+        private static List<Point> GetSameSymbolPoints(ISymbol slotsSymbol, Slots slots)
+        {
+            List<Point> points = new List<Point>();
+
+            const int ignoreFirst = 1;
+            
+            for (int y = 0; y < slots.Rows; y++)
+            {
+                for (int x = ignoreFirst; x < slots.Columns; x++)
+                {
+                    if (slots.Symbols[x, y].GetType() == slotsSymbol.GetType())
+                    {
+                        points.Add(new Point(x, y));
+                    }
+                }
+            }
+
+            return points;
+        }
     }
 }
