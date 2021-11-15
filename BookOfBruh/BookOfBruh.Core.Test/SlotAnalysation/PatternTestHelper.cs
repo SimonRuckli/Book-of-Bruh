f98b72beb257080a758e2e57877e7eb4b2d8eb26
Vincent Dittli <vincent.dittli@bbv.ch>
2021-11-15 23:38:22 +0100

    implemented PatternsFromStringPatterns

--- BookOfBruh/BookOfBruh.Core.Test/SlotAnalysation/PatternTestHelper.cs
+++ BookOfBruh/BookOfBruh.Core.Test/SlotAnalysation/PatternTestHelper.cs
@@ -1,13 +1,39 @@
-﻿using System.Collections.Generic;
-using BookOfBruh.Core.SlotAnalysation;
+﻿using System.Drawing;
 
 namespace BookOfBruh.Core.Test.SlotAnalysation
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using BookOfBruh.Core.SlotAnalysation;
+
     public class PatternTestHelper
     {
         public static List<Pattern> PatternsFromStringPatterns(string[] stringPatterns)
         {
-            throw new System.NotImplementedException();
+            return stringPatterns.Select(PatternFromString).ToList();
+        }
+
+        private static Pattern PatternFromString(string pattern)
+        {
+            const char point = 'P';
+
+            List<List<char>> enumerable = pattern.Split('|', StringSplitOptions.RemoveEmptyEntries).Select(line => line.Select(c => c).ToList()).ToList();
+
+            List<Point> validPoints = new List<Point>();
+
+            for (int y = 0; y < enumerable.Count; y++)
+            {
+                for (int x = 0; x < enumerable[y].Count; x++)
+                {
+                    if (enumerable[y][x] == point)
+                    {
+                        validPoints.Add(new Point(x, y));
+                    }
+                }
+            }
+
+            return new Pattern(validPoints);
         }
     }
 }
