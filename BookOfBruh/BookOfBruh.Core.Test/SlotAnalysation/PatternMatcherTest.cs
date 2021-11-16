5a1e914f2e2fbd9b33a734e37b9eb7b8e364e1db
JoelTschopp <joel_tschopp@sluz.ch>
2021-11-16 16:23:26 +0100

    started PatternMatcher

---
+++ BookOfBruh/BookOfBruh.Core.Test/SlotAnalysation/PatternMatcherTest.cs
@@ -0,0 +1,28 @@
+﻿namespace BookOfBruh.Core.Test.SlotAnalysation
+{
+    using Xunit;
+    using FluentAssertions;
+    using BookOfBruh.Core.SlotAnalysation;
+    using System.Collections.Generic;
+    using System.Drawing;
+
+    public class PatternMatcherTest
+    {
+        [Fact]
+        public void PatternMatcherShould()
+        {
+            // Arrange
+            List<Point> pointList = new List<Point>()
+            {
+                new Point() { X = 0, Y = 0 },
+                new Point() { X = 1, Y = 0 },
+                new Point() { X = 2, Y = 0 }
+            };
+            
+            Pattern testee = new Pattern(pointList);
+            // Act
+            List<Pattern> result = testee.FindMatches(pointList);
+            // Assert
+        }
+    }
+}
--- BookOfBruh/BookOfBruh.Core/SlotAnalysation/PatternMatcher.cs
+++ BookOfBruh/BookOfBruh.Core/SlotAnalysation/PatternMatcher.cs
@@ -1,20 +1,25 @@
-﻿using System.Drawing;
-
-namespace BookOfBruh.Core.SlotAnalysation
+﻿namespace BookOfBruh.Core.SlotAnalysation
 {
+    using System.Drawing;
     using System.Collections.Generic;
+    
     public interface IPatternMatcher
     {
         public List<Pattern> FindMatches(List<Point> input);
     }
 
-    public class Pattern
+    public class Pattern : IPatternMatcher
     {
         public List<Point> Value { get; }
 
         public Pattern(List<Point> value)
         {
             this.Value = value;
+        }
+
+        public List<Pattern> FindMatches(List<Point> input)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
