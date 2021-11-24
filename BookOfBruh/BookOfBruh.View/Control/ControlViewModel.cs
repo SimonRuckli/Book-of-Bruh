c11143a5a7c542f70ecbaa46e23df68435d94026
vincent dittli <vincent.dittli@bbv.ch>
2021-11-24 15:40:28 +0100

    added proof of Concept Command Binding

--- BookOfBruh/BookOfBruh.View/Control/ControlViewModel.cs
+++ BookOfBruh/BookOfBruh.View/Control/ControlViewModel.cs
@@ -1,9 +1,29 @@
 ﻿namespace BookOfBruh.View.Control
 {
     using Infrastructure;
+    using Infrastructure.Commands;
 
     public class ControlViewModel : NotifyPropertyChangedBase
     {
+
+        public ControlViewModel()
+        {
+            this.SpinClickCommand = new RelayCommand(this.SpinClick, this.SpinIsValid);
+        }
+
+        public RelayCommand SpinClickCommand { get; set; }  
+
         public string Guthaben { get; } = "Proof of Concept";
+
+        private bool SpinIsValid()
+        {
+            return true;
+        }
+
+        private void SpinClick()
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
