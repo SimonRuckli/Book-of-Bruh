9bb15de15bebd2d13605cdb2d8610fb5535de6ed
vincent dittli <vincent.dittli@bbv.ch>
2021-12-02 11:10:58 +0100

    added StakeViewModel

---
+++ BookOfBruh/BookOfBruh.View/Stake/StakeEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace BookOfBruh.View.Stake
+{
+    using System;
+
+    public class StakeEventArgs : EventArgs
+    {
+        public StakeEventArgs(double stake)
+        {
+            this.Stake = stake
+        }
+        public double Stake { get; }
+    }
+}
---
+++ BookOfBruh/BookOfBruh.View/Stake/StakeViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace BookOfBruh.View.Stake
+{
+    using System;
+    using Infrastructure.Commands;
+
+    public class StakeViewModel
+    {
+        public StakeViewModel()
+        {
+            this.SelectStakeOneClickCommand = new RelayCommand(this.SelectStakeOneClick, this.SelectStakeOneIsValid);
+        }
+
+        public RelayCommand SelectStakeOneClickCommand { get; set; }
+
+        public EventHandler StakeChanged;
+
+        private bool SelectStakeOneIsValid()
+        {
+            return true;
+        }
+
+        private void SelectStakeOneClick()
+        {
+            this.RaiseStakeChangedEventWithArgs(1);
+        }
+
+        private void RaiseStakeChangedEventWithArgs(double stake)
+        {
+
+            this.StakeChanged.Invoke(this, new StakeEventArgs(stake));
+        }
+    }
+}
