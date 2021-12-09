5c59a7e9626b8f4cb8b9aa6dd3c14f4ee6073bd0
vincent dittli <vincent.dittli@bbv.ch>
2021-12-09 10:31:41 +0100

    added ViewModel and updated Bindings

---
+++ BookOfBruh/BookOfBruh.View/Wallet/WalletViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BookOfBruh.View.Wallet
+{
+    using Core;
+
+    public class WalletViewModel
+    {
+        private readonly Game game;
+
+        public WalletViewModel(Game game)
+        {
+            this.game = game;
+        }
+    }
+}
