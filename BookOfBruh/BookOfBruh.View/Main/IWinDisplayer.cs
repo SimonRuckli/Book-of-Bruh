01f42f3c4ba1b2fad9dc1696c44b3911f214e9d3
Vincent Dittli <vincent.dittli@bbv.ch>
2021-12-12 00:06:57 +0100

    updated Message

--- BookOfBruh/BookOfBruh.View/Main/IWinDisplayer.cs
+++ BookOfBruh/BookOfBruh.View/Main/IWinDisplayer.cs
@@ -17,8 +17,10 @@
         public void Display(double win)
         {
             const int milliseconds = 1000;
+            
 
-            this.timedMessageBox.ShowFor("Gewinn!",$"Du hast {win} gewonnen", milliseconds);
+            this.timedMessageBox.ShowFor("Gewinn!",$"Du hast {win} Bruhcoins gewonnen", milliseconds);
+            
         }
     }
 }
