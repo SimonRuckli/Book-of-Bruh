7c788481c5baa65e039ba016b84d4e02805553fa
Vincent Dittli <vincent.dittli@bbv.ch>
2021-12-13 00:06:48 +0100

    added all Symbol Images

--- BookOfBruh/BookOfBruh.View/Slot/SlotViewModel.cs
+++ BookOfBruh/BookOfBruh.View/Slot/SlotViewModel.cs
@@ -1,5 +1,6 @@
 ﻿namespace BookOfBruh.View.Slot
 {
+    using System;
     using Core.GameData;
     using Core.Symbols;
     using Infrastructure;
@@ -7,24 +8,30 @@
     public class SlotViewModel : NotifyPropertyChangedBase
     {
         public Slots Slots { get; set; }
+        
+        public string Symbol00 => SymbolToPath(this.Slots?.Symbols[0, 0]);
+        public string Symbol10 => SymbolToPath(this.Slots?.Symbols[1, 0]);
+        public string Symbol20 => SymbolToPath(this.Slots?.Symbols[2, 0]);
+        public string Symbol30 => SymbolToPath(this.Slots?.Symbols[3, 0]);
+        public string Symbol40 => SymbolToPath(this.Slots?.Symbols[4, 0]);
 
-        public string Symbol00 => SymbolToString(this.Slots?.Symbols[0, 0]);
-        public string Symbol10 => SymbolToString(this.Slots?.Symbols[1, 0]);
-        public string Symbol20 => SymbolToString(this.Slots?.Symbols[2, 0]);
-        public string Symbol30 => SymbolToString(this.Slots?.Symbols[3, 0]);
-        public string Symbol40 => SymbolToString(this.Slots?.Symbols[4, 0]);
+        public string Symbol01 => SymbolToPath(this.Slots?.Symbols[0, 1]);
+        public string Symbol11 => SymbolToPath(this.Slots?.Symbols[1, 1]);
+        public string Symbol21 => SymbolToPath(this.Slots?.Symbols[2, 1]);
+        public string Symbol31 => SymbolToPath(this.Slots?.Symbols[3, 1]);
+        public string Symbol41 => SymbolToPath(this.Slots?.Symbols[4, 1]);
 
-        public string Symbol01 => SymbolToString(this.Slots?.Symbols[0, 1]);
-        public string Symbol11 => SymbolToString(this.Slots?.Symbols[1, 1]);
-        public string Symbol21 => SymbolToString(this.Slots?.Symbols[2, 1]);
-        public string Symbol31 => SymbolToString(this.Slots?.Symbols[3, 1]);
-        public string Symbol41 => SymbolToString(this.Slots?.Symbols[4, 1]);
+        public string Symbol02 => SymbolToPath(this.Slots?.Symbols[0, 2]);
+        public string Symbol12 => SymbolToPath(this.Slots?.Symbols[1, 2]);
+        public string Symbol22 => SymbolToPath(this.Slots?.Symbols[2, 2]);
+        public string Symbol32 => SymbolToPath(this.Slots?.Symbols[3, 2]);
+        public string Symbol42 => SymbolToPath(this.Slots?.Symbols[4, 2]);
 
-        public string Symbol02 => SymbolToString(this.Slots?.Symbols[0, 2]);
-        public string Symbol12 => SymbolToString(this.Slots?.Symbols[1, 2]);
-        public string Symbol22 => SymbolToString(this.Slots?.Symbols[2, 2]);
-        public string Symbol32 => SymbolToString(this.Slots?.Symbols[3, 2]);
-        public string Symbol42 => SymbolToString(this.Slots?.Symbols[4, 2]);
+        private string SymbolToPath(ISymbol slotsSymbol)
+        {
+            string symbolName = slotsSymbol?.GetType().Name;
+            return $"pack://application:,,,/BookOfBruh.View;component/Images/{symbolName}.png";
+        }
 
         private static string SymbolToString(ISymbol slotsSymbol)
         {
